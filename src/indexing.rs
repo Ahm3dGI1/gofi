e14a646efeca91a6353f197aba54d0ccec2e4c24
Ahmed Ibrahim <ahmedgouda797@gmail.com>
2025-05-17 20:10:04 +0300

    new indexing

--- src/indexing.rs
+++ src/indexing.rs
@@ -1,11 +1,29 @@
-use std::error::Error;
+use std::{error::Error, path::PathBuf};
 use walkdir::WalkDir;
 
-pub fn get_paths(directory: String) -> Result<Vec<std::path::PathBuf>, Box<dyn Error>> {
+#[derive(Debug, Clone)]
+struct IndexedPath {
+    full_path: PathBuf,
+    full_name: String,
+    parent_folder: String,
+}
+
+pub fn get_paths(directory: String) -> Result<Vec<IndexedPath>, Box<dyn Error>> {
     let mut paths = Vec::new();
     for entry in WalkDir::new(directory).into_iter().filter_map(|e| e.ok()) {
         if entry.file_type().is_file() {
-            paths.push(entry.path().to_path_buf());
+            let curr_path = IndexedPath {
+                full_path: entry.path().to_path_buf(),
+                full_name: entry.file_name().to_string_lossy().to_string(),
+                parent_folder: entry
+                    .path()
+                    .parent()
+                    .and_then(|p| p.to_str())
+                    .unwrap_or("")
+                    .to_string(),
+            };
+
+            paths.push(curr_path);
         }
     }
     Ok(paths)
